2933c8f41e6f7c536d5f6f8d4b52351f4072a0d9
Marcus9512 <marcus.jonssonewerbring@gmail.com>
2020-11-19 20:44:49 +0100

    Buggfix

--- Assignment_3/ex_1/hw3_ex1.cu
+++ Assignment_3/ex_1/hw3_ex1.cu
@@ -462,8 +462,8 @@ __global__ void gpu_sobel(int width, int height, float *image, float *image_out)
     //From tutorial in shared memory
     int shared_pos = threadIdx.y * BLOCK_SIZE_SH + threadIdx.x;
 
-	bool less_then_width = idx < (width - 2)
-	bool less_then_height = idy < (height - 2)
+	bool less_then_width = idx < (width - 2);
+	bool less_then_height = idy < (height - 2);
 
 	sh_block[shared_pos] = image[offset_t];
 
@@ -576,7 +576,7 @@ int main(int argc, char **argv)
     {
         // Launch the CPU version
         gettimeofday(&t[0], NULL);
-        //cpu_gaussian(bitmap.width, bitmap.height, image_out[0], image_out[1]);
+        cpu_gaussian(bitmap.width, bitmap.height, image_out[0], image_out[1]);
         gettimeofday(&t[1], NULL);
         
         elapsed[0] = get_elapsed(t[0], t[1]);
@@ -600,7 +600,7 @@ int main(int argc, char **argv)
     {
         // Launch the CPU version
         gettimeofday(&t[0], NULL);
-        //cpu_sobel(bitmap.width, bitmap.height, image_out[1], image_out[0]);
+        cpu_sobel(bitmap.width, bitmap.height, image_out[1], image_out[0]);
         gettimeofday(&t[1], NULL);
         
         elapsed[0] = get_elapsed(t[0], t[1]);
